ccdc9671cb43ac967f2bde032b0acb63646ad15f
Austine Gomez <austine.gomez@yahoo.com>
2023-02-25 21:42:59 -0600

    remove

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: "mongodb+srv://agomez99:AGdeveloper999@cluster0.i1pzftq.mongodb.net/test" ||MONGODB_URI
+  url: "mongodb+srv://agomez99:AGdeveloper999@cluster0.i1pzftq.mongodb.net/test"
 };
