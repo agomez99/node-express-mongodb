56047643196163f64cf31b9f9066b07c243f5144
Austine Gomez <austine.gomez@yahoo.com>
2023-02-25 22:30:58 -0600

    Update server.js

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get('/', function (req,res) {
   res.sendFile(path + "index.html");
 });
 
-require("./app/routes/turorial.routes")(app);
+require("./app/routes/tutrorial.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
