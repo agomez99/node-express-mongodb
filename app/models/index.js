dca7412ab869a876599d80ad54a054b280e1af40
Austine Gomez <austine.gomez@yahoo.com>
2023-02-25 20:23:26 -0600

    Update index.js

--- app/models/index.js
+++ app/models/index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 db.mongoose = mongoose;
-db.url = dbConfig.url || dbConfig.MONGODB_URI;
+db.url = dbConfig.url;
 db.tutorials = require("./tutorial.model.js")(mongoose);
 
 module.exports = db;
